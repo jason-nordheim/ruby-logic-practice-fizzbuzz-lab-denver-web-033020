9822cf26b9b6436230ceb52f467357977efdcb91
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-06 23:20:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(num)
-
+def fizz_3(num)
+  if num % 3 == 0
+    "Fizz"
+  end
 end
